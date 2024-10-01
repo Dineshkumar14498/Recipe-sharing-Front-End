36805e03c92cf3fe70222d93a98ba59d3c0e1c2b
Dineshkumar14498 <dineshvlr9500@gmail.com>
2024-10-01 17:53:56 +0530

    update

--- src/Pages/RecipeDetails.jsx
+++ src/Pages/RecipeDetails.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import profileImg from '../assets/profile.png'
 import { useLoaderData } from 'react-router-dom'
 import {apiUrl} from '../api'
+
 export default function RecipeDetails() {
     const recipe=useLoaderData()
     console.log(recipe)
@@ -13,7 +14,7 @@ export default function RecipeDetails() {
             <h5>{recipe.email}</h5>
         </div>
         <h3 className='title'>{recipe.title}</h3>
-        <img src={apiUrl +`images/${recipe.coverImage}`} width="220px" height="200px"></img>
+        <img src={apiUrl + `images/${recipe.coverImage}`} width="220px" height="200px"></img>
         <div className='recipe-details'>
             <div className='ingredients'><h4>Ingredients</h4><ul>{recipe.ingredients.map(item=>(<li>{item}</li>))}</ul></div>
             <div className='instructions'><h4>Instructions</h4><span>{recipe.instructions}</span></div>
